5b5183272b95d2ed90963526843af61237e2046e
eliazardsuntono <eliazardsunton@gmail.com>
2025-02-07 12:21:39 -0500

    added finishing touches

--- Confession-Website/src/App.jsx
+++ Confession-Website/src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import FirstMessage from './components/FirstMessage'
+import SecondMessage from './components/SecondMessage'
+import ThirdMessage from './components/ThirdMessage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentMessage, setCurrentMessage] = useState(1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handleNextMessage = () => {
+    setCurrentMessage((prev) => prev + 1);
+  }
+
+  const renderMessage = () => {
+    switch (currentMessage) {
+      case 1:
+        return <FirstMessage onContinue={handleNextMessage} />;
+      case 2:
+        return <SecondMessage onContinue={handleNextMessage} />;
+      case 3:
+        return <ThirdMessage/>
+      default:
+        return <h1>Something went wrong!</h1>;
+    }
+  };
+
+  return <div className="App">{renderMessage()}</div>;
 }
 
 export default App
